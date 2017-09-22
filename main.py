2568a826e81ebac247bef3f77cbb288a6c655a37
Aneesh Sachdeva <aneesh@aneeshs-mbp.dhcp.wustl.edu>
2017-09-21 20:45:10 -0500

    Output filepath configured.

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ base_config = lib.load_yaml('base_config')
 if __name__ == '__main__':
     medic_directory = pd.read_csv(base_config['medic_directory_file_path'])
     scheduler = Scheduler(medic_directory=medic_directory, timezone='US/Central')
-    output_file_name = 'test'
+    output_file_name = 'schedule'
     scheduler.run(output_file_path='{}/{}'.format(base_config['output_directory'], output_file_name))
     # test_time = datetime.datetime(2017, 8, 22, 8, 17)
     # now_time = datetime.datetime.utcnow()
